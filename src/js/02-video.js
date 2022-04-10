870aa02315b0d11516f926dec6d478f7a98f2e86
Mariia Telehonenko <telegonenko1@ukr.net>
2022-04-10 22:14:33 +0300

    add 02.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+
+const STORAGE_KEY = "videoplayer-current-time";
+
+const secondsVimeo = localStorage.getItem(STORAGE_KEY);
+
+if (secondsVimeo) {
+player.setCurrentTime(JSON.parse(secondsVimeo));
+};
+
+player.on('timeupdate', throttle(({seconds}) =>{
+
+ localStorage.setItem(STORAGE_KEY, JSON.stringify(seconds)) ;
+   
+}, 1000));
 
