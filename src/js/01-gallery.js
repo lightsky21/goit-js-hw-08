6c6d0ca92c31bcebd09bdbe737b64ae470015673
Mariia Telehonenko <telegonenko1@ukr.net>
2022-04-10 17:59:00 +0300

    add lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryEl = document.querySelector('.gallery');
+
+
+function makeGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join('')
+    
+  
+}
+
+const GalleryItemsMarkup = makeGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', GalleryItemsMarkup); 
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+})
+
